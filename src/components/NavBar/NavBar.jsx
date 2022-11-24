b665e6784dd5e0dc5c2c511c73cbae47dce6a247
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2022-11-23 23:32:14 -0500

    Set up Login & logout UI

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -21,32 +21,18 @@ export default function NavBar({ user, setUser }) {
   //   </nav>
   // );
   return(
-    <div id="header">
-    <div className="d-flex flex-column">
-
-      <div className="profile">
-        {!user &&
-        <Link to="/AdminLogin">Login</Link>
-        }
-        {/* <img src={profilePic} alt="No image" className="img-fluid rounded-circle"> </img> */}
-        <h1 className="text-light"><Link to="">Lukmon Ayanleye</Link></h1>
-        <div className="social-links mt-3 text-center">
-          <a href="https://github.com/Tosin0814" target="_blank" className="github"><i className="bx bxl-github"></i></a>
-          <a href="https://www.linkedin.com/in/oluwatosin-ayanleye/" target="_blank" className="linkedin"><i className="bx bxl-linkedin"></i></a>
-        </div>
-      </div>
-
-      <nav id="navbar" className="nav-menu navbar">
+      <nav id="navbar" className="NavBar nav-menu navbar">
         <ul>
           <li><Link to="#hero" className="nav-link scrollto active"><i className="bx bx-home"></i> <span>Home</span></Link></li>
           <li><Link to="#about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>About</span></Link></li>
           <li><Link to="#resume" className="nav-link scrollto"><i className="bx bx-file-blank"></i> <span>Resume</span></Link></li>
           <li><Link to="#portfolio" className="nav-link scrollto"><i className="bx bx-book-content"></i> <span>Projects</span></Link></li>
           <li><Link to="#contact" className="nav-link scrollto"><i className="bx bx-envelope"></i> <span>Contact</span></Link></li>
-          <li><Link to="" onClick={handleLogOut} className="nav-link"><i className="bx bx-log-out"></i> <span>Logout</span></Link></li>
+          <li>{!user ?
+            <Link to="/AdminLogin" className="nav-link"><i className="bx bx-log-in"></i> <span>Login</span></Link> : 
+            <Link to="" onClick={handleLogOut} className="nav-link"><i className="bx bx-log-out"></i> <span>Logout</span></Link>
+            }</li>
         </ul>
       </nav>
-    </div>
-  </div>
   )
 }
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -6,23 +6,42 @@ import { Routes, Route } from 'react-router-dom';
 import AuthPage from '../AuthPage/AuthPage';
 import ProjectDetailPage from '../ProjectDetailPage/ProjectDetailPage';
 import NavBar from '../../components/NavBar/NavBar';
+import About from '../../components/About/About'
+import Contact from '../../components/Contact/Contact'
+import Home from '../../components/Home/Home'
+import Portfolio from '../../components/Portfolio/Portfolio'
+import Resume from '../../components/Resume/Resume'
+import SkillsList from '../../components/SkillsList/SkillsList'
 
 // Helpers
 import { getUser } from '../../utilities/services/users';
 
-import './App.css';
+import '../../assets/css/style.css';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
+      <header id="header">
+        <div className="d-flex flex-column">
+          <div className="profile">
+            {/* <img src={profilePic} alt="No image" className="img-fluid rounded-circle"> </img> */}
+            <h1 className="text-light">Lukmon Ayanleye</h1>
+            <div className="social-links mt-3 text-center">
+              <a href="https://github.com/Tosin0814" target="_blank" className="github"><i className="bx bxl-github"></i></a>
+              <a href="https://www.linkedin.com/in/oluwatosin-ayanleye/" target="_blank" className="linkedin"><i className="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
           <NavBar user={user} setUser={setUser}/>
-            <Routes>
-              {/* Route components in here */}
-              <Route path="/projects/:projectName" element={<ProjectDetailPage />} />
-              <Route path="/AdminLogin" element={<AuthPage user={user} setUser={setUser} />} />
-            </Routes>
+        </div>
+      </header>
+        <Routes>
+          {/* Route components in here */}
+          <Route path="/projects/:projectName" element={<ProjectDetailPage />} />
+          <Route path="/AdminLogin" element={<AuthPage user={user} setUser={setUser} />} />
+        </Routes>
+      <Home user={user}/>
     </main>
   );
 }
