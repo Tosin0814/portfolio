cef8cb1a297773fa447f852ce136e9f7e6d3bc0f
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2022-11-26 15:09:09 -0500

    Style Portfolio Details Page

--- src/pages/ProjectDetailPage/ProjectDetailPage.jsx
+++ src/pages/ProjectDetailPage/ProjectDetailPage.jsx
@@ -13,7 +13,7 @@ export default function ProjectDetailPage({user}) {
             <h2>Project Details</h2>
             <ol>
               <li><Link to="/">Home</Link></li>
-              <li>Project Name</li>
+              <li>Project Title</li>
             </ol>
           </div>
         </div>
@@ -29,6 +29,23 @@ export default function ProjectDetailPage({user}) {
                   </div>
                   <div className="swiper-pagination"></div>
                   <PortfolioItemImageForm user={user}/>
+                </div>
+              </div>
+
+              <div className="col-lg-4">
+                <div className="portfolio-info">
+                  <h3>Project information</h3>
+                  <ul>
+                    <li><strong>Category</strong>: Web design</li>
+                    <li><strong>Project Date</strong>: 01 March, 2020</li>
+                    <li><strong>GitHub</strong>: <a href={"#"} target="_blank">www.example.com</a></li>
+                    <li><strong>Project URL</strong>: <a href={"#"} target="_blank">www.example.com</a></li>
+                  </ul>
+                </div>
+                <div className="portfolio-description">
+                  <p>
+                    Description: Autem ipsum nam porro corporis rerum. Quis eos dolorem eos itaque inventore commodi labore quia quia. Exercitationem repudiandae officiis neque suscipit non officia eaque itaque enim. Voluptatem officia accusantium nesciunt est omnis tempora consectetur dignissimos. Sequi nulla at esse enim cum deserunt eius.
+                  </p>
                 </div>
               </div>
             </div>
