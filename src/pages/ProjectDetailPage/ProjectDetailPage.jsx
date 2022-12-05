03c0abc55fefbb0e23eec5e9dec2bcd22fe55ec8
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2022-12-05 11:06:32 -0500

    Make portfolio details page more responsive

--- src/pages/ProjectDetailPage/ProjectDetailPage.jsx
+++ src/pages/ProjectDetailPage/ProjectDetailPage.jsx
@@ -48,7 +48,7 @@ export default function ProjectDetailPage({user, portfolioItems, setPortfolioIte
   
 
   return (
-    <main>
+    <main id="main">
       <section id="breadcrumbs" className="breadcrumbs">
         <div className="container">
           <div className="d-flex justify-content-between align-items-center">
