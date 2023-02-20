56a9f766350551279d9c0faee3167f037dd564f8
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2023-02-20 10:26:34 -0500

    Remove 'main' id from ProjectDetailPage

--- src/pages/ProjectDetailPage/ProjectDetailPage.jsx
+++ src/pages/ProjectDetailPage/ProjectDetailPage.jsx
@@ -48,7 +48,7 @@ export default function ProjectDetailPage({user, portfolioItems, setPortfolioIte
   
 
   return (
-    <main id="main">
+    <main id="">
       <section id="breadcrumbs" className="breadcrumbs">
         <div className="container">
           <div className="d-flex justify-content-between align-items-center">
